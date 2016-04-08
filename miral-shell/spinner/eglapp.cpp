dfb03e44c8027718d9e82ba806ca1f707d4b44e7
Alan Griffiths <alan@octopull.co.uk>
2016-04-08 15:33:31 +0100

    Quck and dirty import of old spinner

---
+++ miral-shell/spinner/eglapp.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright © 2013, 2015 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "eglapp.h"
+
+#include "miregl.h"
+
+
+
+float mir_eglapp_background_opacity = 1.0f;
+
+static const char appname[] = "eglspinner";
+
+
+namespace
+{
+template<typename ActiveOutputHandler>
+void for_each_active_output(
+    MirConnection* const connection, ActiveOutputHandler const& handler)
+{
+    /* eglapps are interested in the screen size, so
+    use mir_connection_create_display_config */
+    MirDisplayConfiguration* display_config =
+        mir_connection_create_display_config(connection);
+
+    for (MirDisplayOutput* output = display_config->outputs;
+         output != display_config->outputs + display_config->num_outputs;
+         ++output)
+    {
+        if (output->used &&
+            output->connected &&
+            output->num_modes &&
+            output->current_mode < output->num_modes)
+        {
+            handler(output);
+        }
+    }
+
+    mir_display_config_destroy(display_config);
+}
+
+MirPixelFormat select_pixel_format(MirConnection* connection)
+{
+    unsigned int format[mir_pixel_formats];
+    unsigned int nformats;
+
+    mir_connection_get_available_surface_formats(
+        connection,
+        (MirPixelFormat*) format,
+        mir_pixel_formats,
+        &nformats);
+
+    auto const pixel_format = (MirPixelFormat) format[0];
+
+    printf("Server supports %d of %d surface pixel formats. Using format: %d\n",
+           nformats, mir_pixel_formats, pixel_format);
+
+    return pixel_format;
+}
+}
+
+std::vector<std::shared_ptr<MirEglSurface>> mir_eglapp_init(MirConnection* const connection)
+{
+    MirSurfaceParameters surfaceparm =
+        {
+            "eglappsurface",
+            0, 0,
+            mir_pixel_format_xbgr_8888,
+            mir_buffer_usage_hardware,
+            mir_display_output_id_invalid
+        };
+
+    EGLint swapinterval = 1;
+
+    if (!mir_connection_is_valid(connection))
+        throw std::runtime_error("Can't get connection");
+
+    auto const pixel_format = select_pixel_format(connection);
+    surfaceparm.pixel_format = pixel_format;
+
+    auto const mir_egl_app = make_mir_eglapp(connection, pixel_format);
+
+    std::vector<std::shared_ptr<MirEglSurface>> result;
+
+    // If a size has been specified just do that
+    if (surfaceparm.width && surfaceparm.height)
+    {
+        result.push_back(std::make_shared<MirEglSurface>(mir_egl_app, surfaceparm, swapinterval));
+        return result;
+    }
+
+    // If an output has been specified just do that
+    if (surfaceparm.output_id != mir_display_output_id_invalid)
+    {
+        for_each_active_output(connection, [&](MirDisplayOutput const* output)
+            {
+                if (output->output_id == surfaceparm.output_id)
+                {
+                    auto const& mode = output->modes[output->current_mode];
+                    surfaceparm.width = mode.horizontal_resolution;
+                    surfaceparm.height = mode.vertical_resolution;
+                }
+            });
+        result.push_back(std::make_shared<MirEglSurface>(mir_egl_app, surfaceparm, swapinterval));
+        return result;
+    }
+
+    // but normally, we're fullscreen on every active output
+    for_each_active_output(connection, [&](MirDisplayOutput const* output)
+        {
+            auto const& mode = output->modes[output->current_mode];
+
+            printf("Active output [%u] at (%d, %d) is %dx%d\n",
+                   output->output_id,
+                   output->position_x, output->position_y,
+                   mode.horizontal_resolution, mode.vertical_resolution);
+
+            surfaceparm.width = mode.horizontal_resolution;
+            surfaceparm.height = mode.vertical_resolution;
+            surfaceparm.output_id = output->output_id;
+            result.push_back(std::make_shared<MirEglSurface>(mir_egl_app, surfaceparm, swapinterval));
+        });
+
+    if (result.empty())
+        throw std::runtime_error("No active outputs found.");
+
+    return result;
+}
---
+++ miral-shell/spinner/eglspinner.cpp
@@ -0,0 +1,424 @@
+/*
+ * Copyright © 2013-2015 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Daniel van Vugt <daniel.van.vugt@canonical.com>
+ *          Mirco Müller <mirco.mueller@canonical.com>
+ *          Alan Griffiths <alan@octopull.co.uk>
+ *          Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include "../splash_screen.h"
+
+#include "eglapp.h"
+#include "miregl.h"
+#include <assert.h>
+#include <glib.h>
+#include <string.h>
+#include <GLES2/gl2.h>
+#include <sys/stat.h>
+#if HAVE_PROPS
+#include <hybris/properties/properties.h>
+#endif
+#include <signal.h>
+
+#include "spinner_glow.h"
+#include "spinner_logo.h"
+
+// this is needed for get_gu() to obtain the grid-unit value
+#define MAX_LENGTH       256
+#define VALUE_KEY        "GRID_UNIT_PX"
+#define VALUE_KEY_LENGTH 12
+#define PROP_KEY         "ro.product.device"
+#define DEFAULT_FILE     "/etc/ubuntu-touch-session.d/android.conf"
+#define FILE_BASE        "/etc/ubuntu-touch-session.d/"
+#define FILE_EXTENSION   ".conf"
+
+int get_gu ()
+{
+    int   gu           = 10; // use 10 as a default value
+    FILE* handle       = NULL;
+    int   i            = 0;
+    int   j            = 0;
+    int   len          = 0;
+    char  line[MAX_LENGTH];
+    char  filename[MAX_LENGTH];
+
+    // get name of file to read from
+    bzero ((void*) filename, MAX_LENGTH);
+    strcpy (filename, FILE_BASE);
+
+    struct stat buf;   
+    if (stat(DEFAULT_FILE, &buf) == 0)
+    {
+        strcpy (filename, DEFAULT_FILE);
+    }
+    else
+    {        
+#ifdef HAVE_PROPS
+        char const* defaultValue = "";
+        char  value[PROP_VALUE_MAX];
+        property_get (PROP_KEY, value, defaultValue);
+        strcat (filename, value);
+#endif
+        strcat (filename, FILE_EXTENSION);
+    }
+
+    // try to open it
+    handle = fopen ((const char*) filename, "r");
+    if (!handle)
+        return gu;
+
+    // read one line at a time
+    while (fgets (line, MAX_LENGTH, handle))
+    {
+        // strip line of whitespaces
+        i = 0;
+        j = 0;
+        len = (int) strlen (line);
+        while (i != len)
+        {
+            if (line[i] != ' ' && line[i] != '\t')
+                line[j++] = line[i];
+            i++;
+        }
+        line[j] = 0;
+
+        // parse the line for GU-value
+        if (!strncmp (line, VALUE_KEY, VALUE_KEY_LENGTH))
+            sscanf (line, VALUE_KEY"=%d", &gu);
+    }
+
+    // clean up
+    fclose (handle);
+
+    return gu;
+}
+
+static GLuint load_shader(const char *src, GLenum type)
+{
+    GLuint shader = glCreateShader(type);
+    if (shader)
+    {
+        GLint compiled;
+        glShaderSource(shader, 1, &src, NULL);
+        glCompileShader(shader);
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if (!compiled)
+        {
+            GLchar log[1024];
+            glGetShaderInfoLog(shader, sizeof log - 1, NULL, log);
+            log[sizeof log - 1] = '\0';
+            printf("load_shader compile failed: %s\n", log);
+            glDeleteShader(shader);
+            shader = 0;
+        }
+    }
+    return shader;
+}
+
+// Colours from http://design.ubuntu.com/brand/colour-palette
+//#define MID_AUBERGINE   0.368627451f, 0.152941176f, 0.31372549f
+//#define ORANGE          0.866666667f, 0.282352941f, 0.141414141f
+//#define WARM_GREY       0.682352941f, 0.654901961f, 0.623529412f
+//#define COOL_GREY       0.2f,         0.2f,         0.2f
+//#define LIGHT_AUBERGINE 0.466666667f, 0.297297297f, 0.435294118f
+//#define DARK_AUBERGINE  0.17254902f,  0.0f,         0.117647059f
+#define BLACK           0.0f,         0.0f,         0.0f
+//#define WHITE           1.0f,         1.0f,         1.0f
+
+template <typename Image>
+void uploadTexture (GLuint id, Image& image)
+{
+    glBindTexture(GL_TEXTURE_2D, id);
+
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 GL_RGBA,
+                 image.width,
+                 image.height,
+                 0,
+                 GL_RGBA,
+                 GL_UNSIGNED_BYTE,
+                 image.pixel_data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+GLuint createShaderProgram(const char* vertexShaderSrc, const char* fragmentShaderSrc)
+{
+    if (!vertexShaderSrc || !fragmentShaderSrc)
+        return 0;
+
+    GLuint vShaderId = 0;
+    vShaderId = load_shader(vertexShaderSrc, GL_VERTEX_SHADER);
+    assert(vShaderId);
+
+    GLuint fShaderId = 0;
+    fShaderId = load_shader(fragmentShaderSrc, GL_FRAGMENT_SHADER);
+    assert(fShaderId);
+
+    GLuint progId = 0;
+    progId = glCreateProgram();
+    assert(progId);
+    glAttachShader(progId, vShaderId);
+    glAttachShader(progId, fShaderId);
+    glLinkProgram(progId);
+
+    GLint linked = 0;
+    glGetProgramiv(progId, GL_LINK_STATUS, &linked);
+    if (!linked)
+    {
+        GLchar log[1024];
+        glGetProgramInfoLog(progId, sizeof log - 1, NULL, log);
+        log[sizeof log - 1] = '\0';
+        printf("Link failed: %s\n", log);
+        return 0;
+    }
+
+    return progId;
+}
+
+typedef struct _AnimationValues
+{
+    double lastTimeStamp;
+    GLfloat angle;
+    GLfloat fadeBackground;
+    GLfloat fadeLogo;
+    GLfloat fadeGlow;
+} AnimationValues;
+
+void
+updateAnimation (GTimer* timer, AnimationValues* anim)
+{
+    if (!timer || !anim)
+        return;
+
+    //1.) 0.0   - 0.6:   logo fades in fully
+    //2.) 0.0   - 6.0:   logo does one full spin 360°
+    //3.) 6.0   - 6.833: glow fades in fully, black-background fades out to 50%
+    //4.) 6.833 - 7.666: glow fades out fully, black-background fades out to 0% 
+    //5.) 7.666 - 8.266: logo fades out fully
+    //8.266..:       now spinner can be closed as all its elements are faded out
+
+    double elapsed = g_timer_elapsed (timer, NULL);
+    double dt = elapsed - anim->lastTimeStamp;
+    anim->lastTimeStamp = elapsed;
+
+    // step 1.)
+    if (elapsed < 0.6f)
+        anim->fadeLogo += 1.6f * dt;
+
+    // step 2.)
+    anim->angle -= (0.017453292519943f * 360.0f / 6.0f) * dt;
+
+    // step 3.) glow
+    if (elapsed > 6.0f && elapsed < 6.833f)
+        anim->fadeGlow += 1.2f * dt;
+
+    // Ignore the following three until we can synchronize with greeter
+
+    // step 3.) background
+    //if (elapsed > 6.0f && elapsed < 6.833f)
+    //    anim->fadeBackground -= 0.6f * dt;
+
+    // step 4.) background
+    //if (elapsed > 7.0f)
+    //    anim->fadeBackground -= 0.6f * dt;
+
+    // step 5.)
+    //if (elapsed > 6.833f)
+    //    anim->fadeLogo -= 1.6f * dt;
+}
+
+namespace
+{
+const char vShaderSrcSpinner[] =
+    "attribute vec4 vPosition;                       \n"
+    "attribute vec2 aTexCoords;                      \n"
+    "uniform float theta;                            \n"
+    "varying vec2 vTexCoords;                        \n"
+    "void main()                                     \n"
+    "{                                               \n"
+    "    float c = cos(theta);                       \n"
+    "    float s = sin(theta);                       \n"
+    "    mat2 m;                                     \n"
+    "    m[0] = vec2(c, s);                          \n"
+    "    m[1] = vec2(-s, c);                         \n"
+    "    vTexCoords = m * aTexCoords + vec2 (0.5, 0.5); \n"
+    "    gl_Position = vec4(vPosition.xy, -1.0, 1.0); \n"
+    "}                                               \n";
+
+const char fShaderSrcGlow[] =
+    "precision mediump float;                             \n"
+    "varying vec2 vTexCoords;                             \n"
+    "uniform sampler2D uSampler;                          \n"
+    "uniform float uFadeGlow;                             \n"
+    "void main()                                          \n"
+    "{                                                    \n"
+    "    vec4 col = texture2D(uSampler, vTexCoords);      \n"
+    "    col = col * uFadeGlow;                           \n"
+    "    gl_FragColor = col;                              \n"
+    "}                                                    \n";
+
+const char fShaderSrcLogo[] =
+    "precision mediump float;                             \n"
+    "varying vec2 vTexCoords;                             \n"
+    "uniform sampler2D uSampler;                          \n"
+    "uniform float uFadeLogo;                             \n"
+    "void main()                                          \n"
+    "{                                                    \n"
+    "    vec4 col = texture2D(uSampler, vTexCoords);      \n"
+    "    col = col * uFadeLogo;                           \n"
+    "    gl_FragColor = col;                              \n"
+    "}                                                    \n";
+
+static volatile sig_atomic_t running = 0;
+
+static void shutdown(int signum)
+{
+    if (running)
+    {
+        running = 0;
+        printf("Signal %d received. Good night.\n", signum);
+    }
+}
+
+bool mir_eglapp_running()
+{
+    return running;
+}
+}
+
+void splash_screen(MirConnection* const connection)
+try
+{
+    GLuint prog[2];
+    GLuint texture[2];
+    GLint vpos[2];
+    GLint theta;
+    GLint fadeGlow;
+    GLint fadeLogo;
+    GLint aTexCoords[2];
+    GLint sampler[2];
+
+    auto const surfaces = mir_eglapp_init(connection);
+
+    if (!surfaces.size()) return;
+
+    running = 1;
+    signal(SIGINT, shutdown);
+    signal(SIGTERM, shutdown);
+
+    double pixelSize = get_gu() * 11.18;
+    const GLfloat texCoordsSpinner[] =
+    {
+        -0.5f, 0.5f,
+        -0.5f, -0.5f,
+        0.5f, 0.5f,
+        0.5f, -0.5f,
+    };
+
+    prog[0] = createShaderProgram(vShaderSrcSpinner, fShaderSrcGlow);
+    prog[1] = createShaderProgram(vShaderSrcSpinner, fShaderSrcLogo);
+
+    // setup proper GL-blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+    glBlendEquation(GL_FUNC_ADD);
+
+    // get locations of shader-attributes/uniforms
+    vpos[0] = glGetAttribLocation(prog[0], "vPosition");
+    aTexCoords[0] = glGetAttribLocation(prog[0], "aTexCoords");
+    theta = glGetUniformLocation(prog[0], "theta");
+    sampler[0] = glGetUniformLocation(prog[0], "uSampler");
+    fadeGlow = glGetUniformLocation(prog[0], "uFadeGlow");
+    vpos[1] = glGetAttribLocation(prog[1], "vPosition");
+    aTexCoords[1] = glGetAttribLocation(prog[1], "aTexCoords");
+    sampler[1] = glGetUniformLocation(prog[1], "uSampler");
+    fadeLogo = glGetUniformLocation(prog[1], "uFadeLogo");
+
+    // create and upload spinner-artwork
+    // note that the embedded image data has pre-multiplied alpha
+    glGenTextures(2, texture);
+    uploadTexture(texture[0], spinner_glow);
+    uploadTexture(texture[1], spinner_logo);
+
+    // bunch of shader-attributes to enable
+    glVertexAttribPointer(aTexCoords[0], 2, GL_FLOAT, GL_FALSE, 0, texCoordsSpinner);
+    glVertexAttribPointer(aTexCoords[1], 2, GL_FLOAT, GL_FALSE, 0, texCoordsSpinner);
+    glEnableVertexAttribArray(vpos[0]);
+    glEnableVertexAttribArray(vpos[1]);
+    glEnableVertexAttribArray(aTexCoords[0]);
+    glEnableVertexAttribArray(aTexCoords[1]);
+    glActiveTexture(GL_TEXTURE0);
+
+    AnimationValues anim = {0.0, 0.0, 1.0, 0.0, 0.0};
+    GTimer* timer = g_timer_new();
+
+    while (mir_eglapp_running())
+    {
+        for (auto const& surface : surfaces)
+            surface->paint([&](unsigned int width, unsigned int height)
+            {
+                GLfloat halfRealWidth = ((2.0 / width) * pixelSize) / 2.0;
+                GLfloat halfRealHeight = ((2.0 / height) * pixelSize) / 2.0;
+
+                const GLfloat vertices[] =
+                    {
+                        halfRealWidth,  halfRealHeight,
+                        halfRealWidth, -halfRealHeight,
+                        -halfRealWidth, halfRealHeight,
+                        -halfRealWidth,-halfRealHeight,
+                    };
+
+                glVertexAttribPointer(vpos[0], 2, GL_FLOAT, GL_FALSE, 0, vertices);
+                glVertexAttribPointer(vpos[1], 2, GL_FLOAT, GL_FALSE, 0, vertices);
+
+                glViewport(0, 0, width, height);
+
+                glClearColor(BLACK, anim.fadeBackground);
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                // draw glow
+                glUseProgram(prog[0]);
+                glBindTexture(GL_TEXTURE_2D, texture[0]);
+                glUniform1i(sampler[0], 0);
+                glUniform1f(theta, anim.angle);
+                glUniform1f(fadeGlow, anim.fadeGlow);
+                glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+                // draw logo
+                glUseProgram(prog[1]);
+                glBindTexture(GL_TEXTURE_2D, texture[1]);
+                glUniform1i(sampler[1], 0);
+                glUniform1f(theta, anim.angle);
+                glUniform1f(fadeLogo, anim.fadeLogo);
+                glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+            });
+
+        // update animation variable
+        updateAnimation(timer, &anim);
+    }
+
+    glDeleteTextures(2, texture);
+    g_timer_destroy (timer);
+}
+catch (std::exception const& x)
+{
+    printf("%s\n", x.what());
+}
