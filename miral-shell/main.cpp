fe619bc0e7c46714f07a32e0fef73ac70b85f080
Alan Griffiths <alan@octopull.co.uk>
2016-04-08 14:28:03 +0100

    Split out client code

--- miral-shell/main.cpp
+++ miral-shell/main.cpp
@@ -24,10 +24,10 @@
 #include "miral/window_management_options.h"
 #include "miral/quit_on_ctrl_alt_bksp.h"
 #include "miral/internal_client.h"
+#include "splash_screen.h"
 
 namespace
 {
-void client_code(MirConnection* connection);
 void server_notification(std::weak_ptr<mir::scene::Session> const /*session*/) {}
 }
 
@@ -44,120 +44,6 @@ int main(int argc, char const* argv[])
             },
             display_configuration_options,
             QuitOnCtrlAltBkSp{},
-            InternalClient{client_code, server_notification}
+            InternalClient{splash_screen, server_notification}
         });
-}
-
-#include <mir_toolkit/mir_client_library.h>
-
-#include <chrono>
-
-static void render_pattern(MirGraphicsRegion *region, uint32_t pf)
-{
-    char *row = region->vaddr;
-    int j;
-
-    for (j = 0; j < region->height; j++)
-    {
-        int i;
-        uint32_t *pixel = (uint32_t*)row;
-
-        for (i = 0; i < region->width; i++)
-        {
-            pixel[i] = pf;
-        }
-
-        row += region->stride;
-    }
-}
-
-static MirPixelFormat find_8888_format(MirPixelFormat *formats, unsigned int num_formats)
-{
-    MirPixelFormat pf = mir_pixel_format_invalid;
-
-    for (unsigned int i = 0; i < num_formats; ++i)
-    {
-        MirPixelFormat cur_pf = formats[i];
-        if (cur_pf == mir_pixel_format_abgr_8888 ||
-            cur_pf == mir_pixel_format_xbgr_8888 ||
-            cur_pf == mir_pixel_format_argb_8888 ||
-            cur_pf == mir_pixel_format_xrgb_8888)
-        {
-            pf = cur_pf;
-            break;
-        }
-    }
-
-    return pf;
-}
-
-static void fill_pattern(uint32_t pattern[2], MirPixelFormat pf)
-{
-    switch(pf)
-    {
-    case mir_pixel_format_abgr_8888:
-    case mir_pixel_format_xbgr_8888:
-        pattern[0] = 0xFF00FF00;
-        pattern[1] = 0xFFFF0000;
-        break;
-
-    case mir_pixel_format_argb_8888:
-    case mir_pixel_format_xrgb_8888:
-        pattern[0] = 0xFF00FF00;
-        pattern[1] = 0xFF0000FF;
-        break;
-
-    default:
-        break;
-    };
-}
-
-namespace
-{
-void client_code(MirConnection* connection)
-{
-    puts("Entering client code");
-    int swapinterval = 1;
-
-    unsigned int const num_formats = 32;
-    MirPixelFormat pixel_formats[num_formats];
-    unsigned int valid_formats;
-    mir_connection_get_available_surface_formats(connection, pixel_formats, num_formats, &valid_formats);
-    MirPixelFormat pixel_format = find_8888_format(pixel_formats, valid_formats);
-
-    MirSurfaceSpec *spec =
-        mir_connection_create_spec_for_normal_surface(connection, 640, 480, pixel_format);
-
-    mir_surface_spec_set_name(spec, __FILE__);
-    mir_surface_spec_set_buffer_usage(spec, mir_buffer_usage_software);
-
-    auto surface = mir_surface_create_sync(spec);
-    mir_surface_spec_release(spec);
-
-
-    mir_surface_set_swapinterval(surface, swapinterval);
-
-    uint32_t pattern[2] = {0};
-    fill_pattern(pattern, pixel_format);
-
-    MirGraphicsRegion graphics_region;
-    int i=0;
-    MirBufferStream *bs = mir_surface_get_buffer_stream(surface);
-
-    auto const time_limit = std::chrono::steady_clock::now() + std::chrono::seconds(3);
-
-    puts("Entering loop");
-    while (std::chrono::steady_clock::now() < time_limit)
-    {
-        mir_buffer_stream_get_graphics_region(bs, &graphics_region);
-        i++;
-        render_pattern(&graphics_region, pattern[i & 1]);
-        mir_buffer_stream_swap_buffers_sync(bs);
-    }
-    puts("Exiting loop");
-
-    mir_surface_release_sync(surface);
-
-    puts("Exiting client code");
-}
 }
