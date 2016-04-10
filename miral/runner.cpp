d8e69c6a2947d3f1060c7db5ea1e49a1dc16e209
Alan Griffiths <alan@octopull.co.uk>
2016-04-10 20:27:30 +0100

    Roll the gnome terminal option into the main server

--- miral/runner.cpp
+++ miral/runner.cpp
@@ -20,6 +20,10 @@
 
 #include <mir/server.h>
 #include <mir/report_exception.h>
+#include <mir/options/option.h>
+
+#include <iostream>
+#include <cstring>
 
 namespace
 {
@@ -39,6 +43,49 @@ miral::MirRunner::MirRunner(int argc, char const* argv[], char const* config_fil
 {
 }
 
+namespace
+{
+// TODO There ought to be a better, generic way to specify startup applications
+auto const gnome_terminal = "gnome-terminal";
+
+void enable_startup_applications(::mir::Server& server)
+{
+    server.add_configuration_option(gnome_terminal, "launch gnome-terminal on startup", mir::OptionType::null);
+}
+
+void launch_gnome_terminal()
+{
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        throw std::runtime_error("Failed to fork process");
+    }
+
+    if (pid == 0)
+    {
+        unsetenv("DISPLAY");                                // Discourage toolkits from using X11
+        setenv("GDK_BACKEND", "mir", true);                 // configure GTK to use Mir
+        setenv("QT_QPA_PLATFORM", "ubuntumirclient", true); // configure Qt to use Mir
+        setenv("SDL_VIDEODRIVER", "mir", true);             // configure SDL to use Mir
+
+        char const* exec_args[] = { "gnome-terminal", "--app-id", "com.canonical.miral.Terminal", nullptr };
+
+        sleep(2);
+        execvp(exec_args[0], const_cast<char*const*>(exec_args));
+
+        throw std::logic_error(std::string("Failed to execute client (") + exec_args[0] + ") error: " + strerror(errno));
+    }
+}
+
+void launch_startup_applications(::mir::Server& server)
+{
+    if (auto const options = server.get_options())
+        if (options->is_set(gnome_terminal))
+            launch_gnome_terminal();
+}
+}
+
 auto miral::MirRunner::run_with(std::initializer_list<std::function<void(::mir::Server&)>> options)
 -> int
 try
@@ -47,12 +94,17 @@ try
 
     server.set_config_filename(config_file);
 
+    enable_startup_applications(server);
+
     for (auto& option : options)
         option(server);
 
     // Provide the command line and run the server
     server.set_command_line(argc, argv);
     server.apply_settings();
+
+    launch_startup_applications(server);
+
     server.run();
 
     return server.exited_normally() ? EXIT_SUCCESS : EXIT_FAILURE;
