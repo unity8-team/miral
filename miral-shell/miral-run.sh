b57e8f01c5eafef9889a02701af5b236a38c55a0
Alan Griffiths <alan@octopull.co.uk>
2016-04-12 23:56:10 +0100

    miral-run shouldn't wait for launched process

--- miral-shell/miral-run.sh
+++ miral-shell/miral-run.sh
@@ -2,4 +2,4 @@
 if [ "$1" = "gnome-terminal" ]
 then extras='--app-id com.canonical.miral.Terminal'
 fi
-GDK_BACKEND=mir QT_QPA_PLATFORM=ubuntumirclient SDL_VIDEODRIVER=mir $* $extras
+GDK_BACKEND=mir QT_QPA_PLATFORM=ubuntumirclient SDL_VIDEODRIVER=mir $* $extras&
