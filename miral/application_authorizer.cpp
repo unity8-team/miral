8b86d9ae13d12dec0f495e8627146e597f0ee86a
Alan Griffiths <alan@octopull.co.uk>
2016-06-07 08:42:46 -0400

    ApplicationAuthorizer

---
+++ miral/application_authorizer.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#include "application_authorizer.h"
+
+#include <mir/frontend/session_credentials.h>
+#include <mir/frontend/session_authorizer.h>
+#include <mir/server.h>
+
+namespace mf = mir::frontend;
+
+namespace
+{
+struct SessionAuthorizerAdapter : mf::SessionAuthorizer
+{
+    SessionAuthorizerAdapter(std::shared_ptr<miral::ApplicationAuthorizer> const& app_auth) :
+        app_auth{app_auth} {}
+
+    virtual bool connection_is_allowed(mf::SessionCredentials const& creds) override
+    {
+        return app_auth->connection_is_allowed(creds);
+    }
+
+    virtual bool configure_display_is_allowed(mf::SessionCredentials const& creds) override
+    {
+        return app_auth->configure_display_is_allowed(creds);
+    }
+
+    virtual bool set_base_display_configuration_is_allowed(mf::SessionCredentials const& creds) override
+    {
+        return app_auth->set_base_display_configuration_is_allowed(creds);
+    }
+
+    virtual bool screencast_is_allowed(mf::SessionCredentials const& creds) override
+    {
+        return app_auth->screencast_is_allowed(creds);
+    }
+
+    virtual bool prompt_session_is_allowed(mf::SessionCredentials const& creds) override
+    {
+        return app_auth->prompt_session_is_allowed(creds);
+    }
+
+    std::shared_ptr<miral::ApplicationAuthorizer> const app_auth;
+};
+}
+
+void miral::BasicSetApplicationAuthorizer::operator()(mir::Server& server)
+{
+    server.override_the_session_authorizer([this, &server]()
+        -> std::shared_ptr<mf::SessionAuthorizer>
+        {
+            auto wrapped = builder();
+            my_authorizer = wrapped;
+            return std::make_shared<SessionAuthorizerAdapter>(wrapped);
+        });
+}
+
+
+miral::BasicSetApplicationAuthorizer::BasicSetApplicationAuthorizer(
+    std::function<std::shared_ptr<ApplicationAuthorizer>()> const& builder) :
+    builder{builder}
+{
+}
+
+
+miral::BasicSetApplicationAuthorizer::~BasicSetApplicationAuthorizer() = default;
+
+auto miral::BasicSetApplicationAuthorizer::the_application_authorizer() const -> std::shared_ptr<ApplicationAuthorizer>
+{
+    return my_authorizer.lock();
+}
+
+miral::ApplicationCredentials::ApplicationCredentials(mir::frontend::SessionCredentials const& creds) :
+    creds{creds} {}
+
+auto miral::ApplicationCredentials::pid() const -> pid_t
+{
+    return creds.pid();
+}
+
+auto miral::ApplicationCredentials::uid() const -> uid_t
+{
+    return creds.pid();
+}
+
+auto miral::ApplicationCredentials::gid() const -> gid_t
+{
+    return creds.pid();
+}
