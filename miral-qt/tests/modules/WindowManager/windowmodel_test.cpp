59ebdf27d75e205d5066c5902fa7338dfe1110cb
Gerry Boland <gerry.boland@canonical.com>
2016-08-17 17:31:13 +0100

    Rename SurfaceManager to WindowManager test directory, set up boilerplate for WindowModel testing

---
+++ miral-qt/tests/modules/WindowManager/windowmodel_test.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include <QLoggingCategory>
+#include <QTest>
+#include <QSignalSpy>
+
+// src/common
+#include "windowmodelnotifierinterface.h"
+
+using namespace qtmir;
+
+namespace ms = mir::scene;
+using namespace testing;
+
+
