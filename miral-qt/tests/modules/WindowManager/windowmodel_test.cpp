411013dcb283eee689a51dc7dbbb96ad7fe5b2a7
Gerry Boland <gerry.boland@canonical.com>
2016-08-18 17:52:55 +0100

    Add basic test case framework for qtmir bits

--- miral-qt/tests/modules/WindowManager/windowmodel_test.cpp
+++ miral-qt/tests/modules/WindowManager/windowmodel_test.cpp
@@ -20,12 +20,57 @@
 #include <QTest>
 #include <QSignalSpy>
 
-// src/common
-#include "windowmodelnotifierinterface.h"
+#include "windowmodelnotifier.h"
+#include "Unity/Application/windowmodel.h"
+
+#include "stub_windowmodelnotifier.h"
+
+#include <mir/test/doubles/stub_surface.h>
+#include <mir/test/doubles/stub_session.h>
+
+#include "mirserver/mir/scene/surface_creation_parameters.h"
 
 using namespace qtmir;
 
 namespace ms = mir::scene;
+namespace mg = mir::graphics;
+namespace geom = mir::geometry;
+using StubSurface = mir::test::doubles::StubSurface;
+using StubSession = mir::test::doubles::StubSession;
 using namespace testing;
 
 
+class WindowModelTest : public ::testing::Test
+{
+public:
+    WindowModelTest()
+    {
+        // We don't want the logging spam cluttering the test results
+        QLoggingCategory::setFilterRules(QStringLiteral("qtmir.surfaces=false"));
+    }
+
+    std::shared_ptr<StubSession> const stubSession{std::make_shared<StubSession>()};
+    std::shared_ptr<StubSurface> const stubSurface{std::make_shared<StubSurface>()};
+    miral::Application app{stubSession};
+    miral::Window windowA{app, stubSurface};
+
+    StubWindowModelNotifier m_notifier;
+};
+
+TEST_F(WindowModelTest, AddWindowSucceeds)
+{
+    WindowModel model(&m_notifier, nullptr); // no need for controller in this testcase
+
+    ms::SurfaceCreationParameters windowASpec;
+    windowASpec.of_size(geom::Size{geom::Width{1024}, geom::Height{768}});
+    miral::WindowInfo mirWindowInfo{windowA, windowASpec};
+
+    WindowInfo window{mirWindowInfo};
+
+    QSignalSpy spyCountChanged(&model, SIGNAL(countChanged()));
+
+    m_notifier.emitWindowAdded(window, 0);
+
+    ASSERT_EQ(model.count(), 1);
+    EXPECT_EQ(spyCountChanged.count(), 1);
+}
