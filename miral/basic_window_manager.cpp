d8f17a82087d2e44fde0f07c2f6a626852708a58
Alan Griffiths <alan@octopull.co.uk>
2016-10-05 09:38:12 +0100

    Fix "too clever for own good" code

--- miral/basic_window_manager.cpp
+++ miral/basic_window_manager.cpp
@@ -531,7 +531,7 @@ void miral::BasicWindowManager::move_tree(miral::WindowInfo& root, mir::geometry
 
 void miral::BasicWindowManager::modify_window(WindowInfo& window_info, WindowSpecification const& modifications)
 {
-    std::remove_reference<decltype(window_info)>::type window_info_tmp{window_info};
+    WindowInfo window_info_tmp{window_info};
 
 #define COPY_IF_SET(field)\
     if (modifications.field().is_set())\
