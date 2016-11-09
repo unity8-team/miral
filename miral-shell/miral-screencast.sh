9af83596bca60e052205880efbc1f2c67667d599
Alan Griffiths <alan@octopull.co.uk>
2016-11-09 15:27:43 +0000

    First cut at miral-screencast.sh

---
+++ miral-shell/miral-screencast.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+width=1920
+height=1080
+output=screencast.mp4
+socket=${XDG_RUNTIME_DIR}/mir_socket
+fifofile=$(mktemp)
+
+while [ $# -gt 0 ]
+do
+    if [ "$1" == "--socket" ]; then shift; socket=$1; fi
+    if [ "$1" == "--output" ]; then shift; output=$1; fi
+    if [ "$1" == "--width"  ]; then shift; width=$1;  fi
+    if [ "$1" == "--height" ]; then shift; height=$1; fi
+    shift
+done
+
+echo width = ${width}
+echo height = ${height}
+echo output = ${output}
+echo socket = ${socket}
+echo fifofile = ${fifofile}
+
+if [ -e ${output} ]; then echo "Output exists, moving to ${output}~"; mv ${output} ${output}~ ;fi
+while [ ! -e "${socket}" ]; do echo "waiting for ${socket}"; sleep 1 ;done
+
+mkfifo ${fifofile}
+mirscreencast -n -1 -m ${socket} -f ${fifofile}& mirscreencast_pid=$!
+
+read -rsp $'Press enter when recording complete...\n'
+kill ${mirscreencast_pid}
+
+mencoder -demuxer rawvideo -rawvideo fps=60:w=${width}:h=${height}:format=bgra -ovc x264 -o ${output} ${fifofile}
+rm ${fifofile}
