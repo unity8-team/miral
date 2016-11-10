4f6d1003ffaf95e0e1958a2a98c807654dd90e4e
Alan Griffiths <alan@octopull.co.uk>
2016-11-10 09:33:57 +0000

    Ensure temporary file is deleted

--- miral-shell/miral-screencast.sh
+++ miral-shell/miral-screencast.sh
@@ -3,7 +3,6 @@ width=1920
 height=1080
 output=screencast.mp4
 socket=${XDG_RUNTIME_DIR}/mir_socket
-fifofile=$(mktemp)
 
 while [ $# -gt 0 ]
 do
@@ -18,7 +17,6 @@ echo width = ${width}
 echo height = ${height}
 echo output = ${output}
 echo socket = ${socket}
-echo fifofile = ${fifofile}
 
 if ! which mirscreencast >> /dev/null ; then echo "Need mirscreencast - run \"sudo apt install mir-utils\""; exit 1 ;fi
 if ! which mencoder >> /dev/null ;      then echo "Need mencoder - run \"sudo apt install mencoder\""; exit 1 ;fi
@@ -26,12 +24,13 @@ if ! which mencoder >> /dev/null ;      then echo "Need mencoder - run \"sudo ap
 if [ -e ${output} ]; then echo "Output exists, moving to ${output}~"; mv ${output} ${output}~ ;fi
 while [ ! -e "${socket}" ]; do echo "waiting for ${socket}"; sleep 1 ;done
 
-mkfifo ${fifofile}
-mirscreencast -n -1 -m ${socket} -f ${fifofile}& mirscreencast_pid=$!
+tempfile=$(mktemp)
+trap 'rm -f -- "${tempfile}"; exit 1' INT TERM HUP EXIT
+
+mirscreencast --size ${width} ${height} -m ${socket} -f ${tempfile}& mirscreencast_pid=$!
 
 sleep 1; # don't lose the next message in the spew from mirscreencast
 read -rsp $'\n\nPress enter when recording complete...'
 kill ${mirscreencast_pid}
 
-mencoder -demuxer rawvideo -rawvideo fps=60:w=${width}:h=${height}:format=bgra -ovc x264 -o ${output} ${fifofile}
-rm ${fifofile}
+mencoder -demuxer rawvideo -rawvideo fps=60:w=${width}:h=${height}:format=bgra -ovc x264 -o ${output} ${tempfile}
