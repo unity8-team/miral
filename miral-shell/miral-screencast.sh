955141841809876105ca4356c3e73c81e9e80789
Alan Griffiths <alan@octopull.co.uk>
2016-11-10 15:10:32 +0000

    Tidy up better, and add help text

--- miral-shell/miral-screencast.sh
+++ miral-shell/miral-screencast.sh
@@ -3,14 +3,26 @@ width=1920
 height=1080
 output=screencast.mp4
 socket=${XDG_RUNTIME_DIR}/mir_socket
+if [ -v MIR_SERVER ]; then socket=${MIR_SERVER}; fi
 
 while [ $# -gt 0 ]
 do
-    if [ "$1" == "--socket" ]; then shift; socket=$1; fi
-    if [ "$1" == "--output" ]; then shift; output=$1; fi
-    if [ "$1" == "--width"  ]; then shift; width=$1;  fi
-    if [ "$1" == "--height" ]; then shift; height=$1; fi
-    shift
+  if [ "$1" == "--help" -o "$1" == "-h" ]
+  then
+    echo "$(basename $0) - screencast capture script for use with Mir servers"
+    echo "Usage: $0 [options]"
+    echo "Options are:"
+    echo "    --width   set the capture width   [${width}]"
+    echo "    --height  set the capture height  [${height}]"
+    echo "    --output  set the output filename [${output}]"
+    echo "    --socket  set the mir socket      [${socket}]"
+    exit 0
+  elif [ "$1" == "--socket" ]; then shift; socket=$1
+  elif [ "$1" == "--output" ]; then shift; output=$1
+  elif [ "$1" == "--width"  ]; then shift; width=$1
+  elif [ "$1" == "--height" ]; then shift; height=$1
+  fi
+  shift
 done
 
 echo width = ${width}
@@ -25,12 +37,10 @@ if [ -e ${output} ]; then echo "Output exists, moving to ${output}~"; mv ${outpu
 while [ ! -e "${socket}" ]; do echo "waiting for ${socket}"; sleep 1 ;done
 
 tempfile=$(mktemp)
-trap 'rm -f -- "${tempfile}"; exit 1' INT TERM HUP EXIT
-
 mirscreencast --size ${width} ${height} -m ${socket} -f ${tempfile}& mirscreencast_pid=$!
+trap 'kill ${mirscreencast_pid}; rm -f -- "${tempfile}"; exit 0' INT TERM HUP EXIT
 
 sleep 1; # don't lose the next message in the spew from mirscreencast
 read -rsp $'\n\nPress enter when recording complete...'
-kill ${mirscreencast_pid}
 
 mencoder -demuxer rawvideo -rawvideo fps=60:w=${width}:h=${height}:format=bgra -ovc x264 -o ${output} ${tempfile}
