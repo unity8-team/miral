c965a1dc39e9303e85d4f683f929b84301cf5134
Alan Griffiths <alan@octopull.co.uk>
2017-02-20 16:50:23 +0000

    Some tests

--- test/mru_window_list.cpp
+++ test/mru_window_list.cpp
@@ -30,7 +30,9 @@ namespace
 {
 struct StubSurface : mir::test::doubles::StubSurface
 {
-    bool visible() const override { return true; }
+    bool visible() const override { return visible_; }
+
+    bool visible_ = true;
 };
 
 struct StubSession : mir::test::doubles::StubSession
@@ -57,13 +59,26 @@ MATCHER(IsNullWindow, std::string("is not null"))
 
 struct MRUWindowList : testing::Test
 {
+    static auto const window_a_id = 0;
+    static auto const window_b_id = 1;
+
     miral::MRUWindowList mru_list;
 
     std::shared_ptr<StubSession> const stub_session{std::make_shared<StubSession>(3)};
     miral::Application app{stub_session};
-    miral::Window window_a{app, stub_session->surface(mir::frontend::SurfaceId{0})};
-    miral::Window window_b{app, stub_session->surface(mir::frontend::SurfaceId{1})};
+    miral::Window window_a{app, stub_session->surface(mir::frontend::SurfaceId{window_a_id})};
+    miral::Window window_b{app, stub_session->surface(mir::frontend::SurfaceId{window_b_id})};
     miral::Window window_c{app, stub_session->surface(mir::frontend::SurfaceId{2})};
+
+    void hide_window(int window_id)
+    {
+        stub_session->surfaces[window_id]->visible_ = false;
+    }
+
+    void show_window(int window_id)
+    {
+        stub_session->surfaces[window_id]->visible_ = true;
+    }
 };
 
 TEST_F(MRUWindowList, when_created_is_empty)
@@ -141,3 +156,38 @@ TEST_F(MRUWindowList, when_enumerator_returns_false_enumeration_is_short_circuit
 
     EXPECT_THAT(count, Eq(1));
 }
+
+TEST_F(MRUWindowList, a_hidden_window_is_not_enumerated)
+{
+    mru_list.push(window_a);
+    mru_list.push(window_b);
+    mru_list.push(window_c);
+
+    int count{0};
+
+    hide_window(window_a_id);
+    mru_list.enumerate([&](miral::Window& window)
+       { if (window == window_a) ++count; return true; });
+
+    EXPECT_THAT(count, Eq(0));
+}
+
+TEST_F(MRUWindowList, hiding_then_showing_windows_retains_order)
+{
+    mru_list.push(window_a);
+    mru_list.push(window_b);
+    mru_list.push(window_c);
+
+    hide_window(window_a_id);
+    hide_window(window_b_id);
+    show_window(window_a_id);
+    show_window(window_b_id);
+
+    std::vector<miral::Window> as_enumerated;
+
+    mru_list.enumerate([&](miral::Window& window)
+       { as_enumerated.push_back(window); return true; });
+
+    EXPECT_THAT(as_enumerated, ElementsAre(window_c, window_b, window_a));
+}
+
